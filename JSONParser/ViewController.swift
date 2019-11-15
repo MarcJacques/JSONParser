99acaaea5af939431a5139cac8076793fbd34515
Marc Jacques <Marcjacques@me.com>
2019-11-15 12:49:06 -0500

    Decoded into JSON successfully

--- JSONParser/ViewController.swift
+++ JSONParser/ViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let path = Bundle.main.path(forResource: "usersAPI", ofType: "txt") else { return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            print(json)
+            guard let array = json as? [Any] else { return }
+            
+            for user in array {
+                guard let userDict = user as? [String: Any] else { return }
+                guard let userID = userDict["id"] as? Int else { print("not as Int"); return }
+                guard let name = userDict["name"] as? String else { return }
+                guard let company = userDict["company"] as? [String: String] else { return }
+                guard let companyName = company["name"] else { return }
+                
+                print(userID)
+                print(name)
+                print(companyName)
+                print(" ")
+            }
+    } catch {
+    print(error)
     }
+    
+}
 
 
 }
